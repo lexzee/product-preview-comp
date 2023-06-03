4f1e8d196f7b5fc54818783f390dd49972c6b8f5
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-03 18:37:08 +0100

    Added basic html structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
+import img from './images/image-product-mobile.jpg';
 
 function App() {
   return (
     <div className="App">
-  Preview
-
-Gabrielle Essence Eau De Parfum
-
-A floral, solar and voluptuous interpretation composed by Olivier Polge,
-Perfumer-Creator for the House of CHANEL.
-
-$149.99
-$169.99
-
-Add to Cart
-
-<div class="attribution">
-  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
-  Coded by <a href="https://github.com/lexzee">Your Name Here</a>.
-</div>
+      <div className="cardImg">
+        <img src={img} alt="product" />
+      </div>
+      <div className="cardInfo">
+        <div className="cardInfo__title">
+          <span className="prodType">PERFUME</span>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+        </div>
+        <div className="cardInfo__description">
+          <p>
+          A floral, solar and voluptuous interpretation composed by Olivier Polge,
+    Perfumer-Creator for the House of CHANEL.
+          </p>
+        </div>
+        <div className="cardInfo__price">
+          <h2>$149.99 <span className="prev-price">$169.99</span> </h2>
+        </div>
+        <div className="cardInfo__buttons">
+          <button className="cardInfo__buttons--add">Add to Cart</button>
+        </div>
+      </div>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/lexzee">Your Name Here</a>.
+      </div>
     </div>
   );
 }
